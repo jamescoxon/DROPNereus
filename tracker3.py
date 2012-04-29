05d9151e8e585041719e61064a95d2fa13a9e052
James Coxon <jacoxon@googlemail.com>
2012-04-29 21:18:21 +0100

    new file

---
+++ tracker3.py
@@ -0,0 +1,3 @@
+#Script written in python for the S60
+# Written for the Nokia 6110 Navigator (with a slightly broken screen!)
+# Aim is to have it turn on GPS, get a position, turn off the GPS and then send it by SMS
